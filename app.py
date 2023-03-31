f41133dc68ed44fc5db9c486e2037f4753a54d56
Benoit Favier <benoitfamillefavier@gmail.com>
2023-03-31 20:59:51 +0200

    draft of application

--- app.py
+++ app.py
@@ -1,17 +1,44 @@
 import streamlit as st
 import pygmalion as ml
+import re
+import string
+from typing import List
 
-@st.cache_data
-def load_model():
-    return ml.neural_networks.TextTranslator.load("model.pth")
 
-data_load_state = st.text('Loading model...')
-model = load_model()
-data_load_state.text('Loading model... Done!')
+@st.cache_data(show_spinner=False)
+def download_model():
+    # return ml.neural_networks.TextTranslator.load("model.pth")
+    return ml.neural_networks.TextTranslator.load("https://drive.google.com/file/d/1l4jxLJpLt8xmxf9JMIVM4lYv42erF--0/view?usp=share_link")
 
-st.session_state.input = ""
-st.session_state.translation = ""
 
-st.title("Neural Machine Translation français → anglais")
-input_text_area = st.text_area(label="Le text à traduire:", value=st.session_state.input, placeholder="J'ai toujours voulu être un oiseau ...", max_chars=10000, height=100)
-translation_text_area = st.text_area(label="Traduction:", value=st.session_state, disabled=True)
+def format_sentences(text: str) -> List[str]:
+    """
+    Split an input text into formated input sentences
+    """
+    sentences = re.split("(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s", text)
+    sentences = (s.capitalize().strip() for s in sentences)
+    sentences = (s.replace(" ,", ",").replace(" .", ".") for s in sentences)
+    sentences = (re.sub("(?<=[^\\s\\^]):", " :", re.sub("(?<=[^\\s\\^])\?", " ?", re.sub("(?<=[^\\s\\^])!", " !", s))) for s in sentences)
+    sentences = [s if s.endswith((".", "?", "!", ":")) else s+"." for s in sentences]
+    return sentences
+
+
+with st.spinner(text="Téléchargement du modèle ..."):
+    model = download_model()
+
+st.title("Traduction français → anglais")
+input_text = st.text_area(label="Le text à traduire:", placeholder="J'ai toujours voulu être un vrai petit garçon ...", max_chars=10000, height=100)
+
+if len(input_text) > 0:
+    inputs = format_sentences(input_text)
+    predictions = []
+    progress_bar = st.progress(0., "Traduction en cours ...")
+    for i, input in enumerate(inputs, start=1):
+        predictions.append(model.predict(input, n_beams=3)[0])
+        progress_bar.progress(int(i*100/len(inputs)), "Traduction en cours ...")
+    predictions = ["".join(filter(lambda x: x in string.printable, s)) for s in predictions]  # Filtre les charactères non-affichables
+    progress_bar.empty()
+    print(predictions)
+    st.caption("Traduction:")
+    translation = " ".join(predictions)
+    st.code(translation)
